f212edfc23f38f0d63826bc609364106999200a6
Andres Gonzalez <anfegoca@gmail.com>
2021-07-24 16:27:22 -0500

    Project structure

---
+++ src/main/java/com/masiv/casinoapi/App.java
@@ -0,0 +1,22 @@
+package com.masiv.casinoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collections;
+
+@SpringBootApplication
+public class App{
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(App.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port",getPort()));
+        app.run(args);
+    }
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+
+        return 8080;
+    }
+}
